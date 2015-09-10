f65d93403254278f2b2e80435b3d1b423aac68f0
Bruce Hill <bhill@slac.stanford.edu>
2015-09-10 04:30:00 +0000

    Added support for new SLAC EVR firmware 1FD20005
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/event2/current@22159 ececa60f-9f63-0410-97c7-ea05888eb260

--- mrfApp/src/drvMrfEr.h
+++ mrfApp/src/drvMrfEr.h
@@ -91,6 +91,7 @@
 #define EVR_FIRMWARE_REV_SLAC2  0x1FD00023
 #define EVR_FIRMWARE_REV_SLAC3  0x1FD10023
 #define EVR_FIRMWARE_REV_SLAC4  0x1FD10024
+#define EVR_FIRMWARE_REV_SLAC5  0x1FD20005
 
 /**************************************************************************************************/
 /*  Configuration Constants                                                                       */
