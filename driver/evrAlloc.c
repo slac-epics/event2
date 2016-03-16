8c12247b0e69f66df010c31a5cd57903d2ef8b39
Bruce Hill <bhill@slac.stanford.edu>
2016-03-16 02:33:05 +0000

    Added op types to usage, print fpga version on check all, and made driver build work better w/ multiple users.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/event2/current@24632 ececa60f-9f63-0410-97c7-ea05888eb260

--- driver/evrAlloc.c
+++ driver/evrAlloc.c
@@ -17,6 +17,10 @@ void usage(void)
     printf("       evrAlloc [ -d DEVICE ]\n");
     printf("       evrAlloc -h\n");
     printf("The first form performs an EVR trigger allocation operation.\n");
+    printf("    OP and TRIGGER are integers.\n");
+	printf("    OP: 0=free, 1=allocate, 2=steal, 3=check\n");
+	printf("    TRIGGER: trigger number\n");
+	printf("    Specify a sleep duration in sec to sleep then free the trigger.\n");
     printf("The second form checks ownership of *all* triggers.\n");
     printf("The third form prints this help message.\n");
     exit(0);
@@ -76,11 +80,13 @@ int main(int argc, char **argv)
     }
     fd = open(device, O_RDWR);
     if (i == argc) {
-        len = (READ_EVR_REGISTER(fd, FPGAVersion) >> 24) & 0x0f;
-        if (len == 15)
+		int FPGAVersion = READ_EVR_REGISTER(fd, FPGAVersion);
+		printf( "EVR Found with Firmware Revision 0x%04X\n", FPGAVersion );
+        if ( ((FPGAVersion >> 24) & 0x0f) == 15 )
             len = 12;
         else
             len = 4;
+		printf( "Checking all trigger channels.\n" );
         for (i = 0; i < len; i++) {
             eit.Id = i;
             eit.Op = EvrTrigCheck;
@@ -101,8 +107,8 @@ int main(int argc, char **argv)
         ret = ioctl(fd, EV_IOCTRIG, &eit);
         report(eit.Op, eit.Id, ret);
 
-        if (i + 2 != argc) {
-            sleep(atoi(argv[i+2]));
+        if (stime != 0) {
+            sleep(stime);
             if (eit.Op == EvrTrigAlloc || eit.Op == EvrTrigSteal) {
                 eit.Op = EvrTrigFree;
                 ret = ioctl(fd, EV_IOCTRIG, &eit);
