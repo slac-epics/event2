e6f945cce806dfba32c571c8cb32502a701e10db
Scott A. Stubbs <sstubbs@slac.stanford.edu>
2016-11-01 05:56:28 +0000

    Adding newer SLAC G2 firmware and updating ev2_driver.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/event2/current@27152 ececa60f-9f63-0410-97c7-ea05888eb260

--- mrfApp/src/drvMrfEr.h
+++ mrfApp/src/drvMrfEr.h
@@ -91,6 +91,7 @@
 #define EVR_FIRMWARE_REV_SLAC3  0x1FD10023
 #define EVR_FIRMWARE_REV_SLAC4  0x1FD10024
 #define EVR_FIRMWARE_REV_SLAC5  0x1FD20005
+#define EVR_FIRMWARE_REV_SLAC6  0x1FD20025
 
 /**************************************************************************************************/
 /*  Configuration Constants                                                                       */
