f2e40e2a0d5505ee2c58628c558abcc8794b6549
Bruce Hill <bhill@slac.stanford.edu>
2018-04-19 19:24:01 -0700

    Revised rms variance to use denominator k to compute population variance.
    Prior version was computing sample variance w/ (k-1) denominator.

--- evrSupport/src/bsa.c
+++ evrSupport/src/bsa.c
@@ -263,7 +263,8 @@ int bsaSecnAvg(epicsTimeStamp *secnTime_ps,
         bsa_ps->stdDev = 0.0;
         bsa_ps->rms    = 0.0;
       } else {
-        bsa_ps->var    = bsa_ps->sumVar/(bsa_ps->avgcnt-1);
+        /* Using k denom for population var instead of sample var */
+        bsa_ps->var    = bsa_ps->sumVar/bsa_ps->avgcnt;
         bsa_ps->stdDev = sqrt(bsa_ps->var);
         bsa_ps->rms    = sqrt(bsa_ps->sumSq/bsa_ps->avgcnt);
       }
